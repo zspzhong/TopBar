a3b865862df100a6256b394164293dc204dd4d81
zhongzsp@aliyun.com <zhongzsp@aliyun.com>
2026-03-20 01:31:24 +0800

    Initial Commit

---
+++ TopBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TopBar
+//
+//  Created by ER on 2026/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TopBar/TopBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopBarApp.swift
+//  TopBar
+//
+//  Created by ER on 2026/3/20.
+//
+
+import SwiftUI
+
+@main
+struct TopBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
